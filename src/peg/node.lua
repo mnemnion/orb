fdee9a1685e88ba9df6d9ee4d44a6d22be3f2866
Sam Atman <atmanistan@gmail.com>
2017-09-01 15:55:40 -0700

    Simpler Node metatable
    I was doing some weird thing with a closure
    
    2015 was a weird year

--- src/peg/node.lua
+++ src/peg/node.lua
@@ -1,33 +1,24 @@
--- The Node metatable type
-
--- Note that this should be a singleton: called exactly once by a given program. 
-
--- This can probably just be a table. 
+-- The Node metatable 
 
 local ast = require "peg/ast-node"
 local transform = require "peg/transform"
 
-local function N () 
-  -- <Node> metatable
-  local meta = {}
-  meta["__call"] = function ()
-    io.write "Cannot call Node without evaluator"
-  end
-  meta["__index"] = meta
-  meta["__tostring"] = ast.tostring
-  meta["isnode"] = true
-  meta["root"] = ast.root
-  meta["range"] = ast.range
-  meta["copy"] = ast.copy
-  meta["lift"]  = ast.lift
-  meta["tokens"] = ast.tokenize
-  meta["flatten"] = ast.flatten
-  meta["select"] = ast.__select_node
-  meta["with"] = ast.__select_with_node
-  meta["dot"] = transform.dot
-  return meta
+
+local meta = {}
+meta["__call"] = function ()
+  io.write "Cannot call Node without evaluator"
 end
+meta["__index"] = meta
+meta["__tostring"] = ast.tostring
+meta["isnode"] = true
+meta["root"] = ast.root
+meta["range"] = ast.range
+meta["copy"] = ast.copy
+meta["lift"]  = ast.lift
+meta["tokens"] = ast.tokenize
+meta["flatten"] = ast.flatten
+meta["select"] = ast.__select_node
+meta["with"] = ast.__select_with_node
+meta["dot"] = transform.dot
 
-local N = N()
-
-return N
+return meta
