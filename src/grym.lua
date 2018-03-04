60e6503e848dd43aa2df7542468d0897623f5fa8
Sam Atman <atmanistan@gmail.com>
2018-03-04 13:21:48 -1000

    Add invert.lua to src root

--- src/grym.lua
+++ src/grym.lua
@@ -18,6 +18,7 @@ local ast = require "peg/ast"
 local epeg = require "peg/epeg"
 
 local P_grym = require "grym/grymmyr" 
+local m = require "grym/morphemes"
 
 local grym = {}
 
@@ -37,32 +38,42 @@ local function write_footer()
     return "#/lua\n"
 end
 
+local function isBlank(line)
+    local all_blanks = L.match((m.__TAB__ + m._)^0, line)
+    return (all_blanks == #line or line == "")
+end
+
 local function cat_lines(phrase, lines)
     if type(lines) == "string" then
         return phrase .. lines .. "\n"
     end
-
-    for i = 1, #lines do
-        phrase = phrase .. lines[i] .. "\n"
+    local blanks = {}
+    for _,l in ipairs(lines) do
+        if isBlank(l) then
+            blanks[#blanks + 1] = ""
+        else 
+            phrase = phrase .. l .. "\n"
+        end
     end
 
-    return phrase
+    return phrase, {}
 end
 
 -- inverts a source code file into a grimoire document
 function grym.invert(str)
     local code_block = false
     local phrase = ""
+    local bottom_trim = nil
     local lines = {}
     for _, line in ipairs(epeg.split(str, "\n")) do
         -- Two kinds of line: comment and code. For comment:
         if (L.match(L.P"-- ", line) 
             or (L.match(L.P("--"), line) and #line == 2)) then
             if code_block then
-                phrase = cat_lines(phrase, lines)
+                phrase, bottom_trim = cat_lines(phrase, lines)
                 phrase = cat_lines(phrase,write_footer())
                 code_block = false
-                lines = {}
+                lines = bottom_trim
             end 
             phrase = phrase .. line:sub(4,  -1) .. "\n"
         else
@@ -103,3 +114,4 @@ end
 
 local block = read("../src/grym/block.lua")
 io.write(grym.invert(block))
+
