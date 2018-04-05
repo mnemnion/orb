fe7d31412fb044728efc96d9fb03f989f56e3a7f
Sam Atman <atmanistan@gmail.com>
2018-04-05 10:42:51 -0700

    new markdown files

---
+++ src/grym/richtext.lua
@@ -0,0 +1,65 @@
+
+
+
+
+
+
+
+
+
+
+local Node = require "node"
+
+local u = require "util"
+
+
+
+
+
+
+
+
+
+
+
+local Lit, lit = u.inherit(Node)
+
+
+
+
+
+
+
+function Lit.toMarkdown(literal)
+  return "`" .. literal:toValue() .. "`"
+end
+
+
+
+
+
+local Ita = u.inherit(Node)
+
+function Ita.toMarkdown(italic)
+  return "*" .. italic:toValue() .. "*"
+end
+
+
+
+
+
+local Bold = u.inherit(Node)
+
+function Bold.toMarkdown(bold)
+  return "**" .. bold:toValue() .. "**"
+end
+
+
+
+
+
+
+
+return { literal = Lit, 
+     italic  = Ita,
+     bold    = Bold }
