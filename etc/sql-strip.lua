62cd50cd25ab0508685a7b4b3e5eaac4bda6756f
Sam Atman <atmanistan@gmail.com>
2019-07-25 21:56:02 +0200

    Matches header lines

--- etc/sql-strip.lua
+++ etc/sql-strip.lua
@@ -1,13 +1,25 @@
 -- Let's write this in the language we actually use!
 
 local L = require "lpeg"
-print(os.getenv("HOME") .. "/Dropbox/br/" .. arg[1])
-print(io.open(os.getenv("HOME") .. "/Dropbox/br/orb/" .. arg[1], "r"):read())--(io.open(arg[1])):read()
----[[
+local P, match = L.P, L.match
+
 local file = io.open(os.getenv("HOME") .. "/Dropbox/br/orb/" .. arg[1], "r")
+
+local headmatch = P"*"^1 * (P" ")
+                  --* (-(P"\n") *P(1)^0) * (P"\n")
+
+local function _chunkLine(line)
+    if match(headmatch, line) then
+      return line
+   else
+      return ""
+   end
+
+end
+
 while true do
    local line = file : read()
    if not line then break end
-   print (line)
+   print (_chunkLine(line))
 end
 --]]
