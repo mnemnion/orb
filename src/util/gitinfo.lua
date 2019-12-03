022134d7856924ce84b3735bc808a890af5ca158
Sam Atman <atmanistan@gmail.com>
2019-12-02 18:42:23 -0800

    get remotes info

--- src/util/gitinfo.lua
+++ src/util/gitinfo.lua
@@ -8,15 +8,25 @@ local sh = require "orb:util/sh"
 local pl = require "orb:util/plmini"
 local git = sh.command "git"
 local isdir = pl.path.isdir
+local lines = core.lines
+local insert = assert(table.insert)
 
 local function gitInfo(path)
    local git_info = {}
    if isdir(path.."/.git") then
       git_info.is_repo = true
       local branches = tostring(git "branch")
-      for branch in core.lines(branches) do
+      for branch in lines(branches) do
          if branch:sub(1,1) == "*" then
             git_info.branch = branch:sub(3)
+         end
+      end
+      local remotes = tostring(git "remote")
+      if remotes then
+         git_info.remotes = {}
+         for remote in lines(remotes) do
+            insert(git_info.remotes,
+                  { [remote] = tostring(git ("remote get-url " .. remote)) })
          end
       end
    else
