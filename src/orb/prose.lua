98363a2fe970b19335ac783a8150b11e1fdc524b
Sam Atman <atmanistan@gmail.com>
2020-01-26 01:35:50 -0500

    Complete set of bookended markup rules
    Works well enough to start trying it out on some decently
    marked-up prose.
    
    Tomorrow. Or at least, after I've slept today.

--- src/orb/prose.lua
+++ src/orb/prose.lua
@@ -4,29 +4,65 @@
 
 
 
-
 local Peg = require "espalier:peg"
+
+
+
 local prose_str = [[
-            prose  ←  (escape / italic / bold / raw)+
+               prose  ←  ( escape
+                          / italic
+                          / bold
+                          / strike
+                          / literal
+                          / underline
+                          / raw)+
 
-           escape  ←  "\\" {*/~_=}
+              escape  ←  "\\" {*/~_=}
 
-             bold  ←   bold-start non-bold bold-end
-     `bold-start`  ←  "*"+@bold-c !(" " / "\n")
-       `bold-end`  ←  "*"+@(bold-c)
-       `non-bold`  ←  ( WS+ (!italic !bold-end fill)+
-                       / WS* italic
-                       / (!italic !bold-end fill)+ )+
+                bold  ←   bold-start bold-body bold-end
+        `bold-start`  ←  "*"+@bold-c !WS
+          `bold-end`  ←  "*"+@(bold-c)
+         `bold-body`  ←  ( WS+ (!non-bold !bold-end fill)+
+                          / WS* non-bold
+                          / (!non-bold !bold-end fill)+ )+
+         `non-bold`   ←  italic / strike / underline /literal
 
-           italic  ←  italic-start non-italic italic-end
-   `italic-start`  ←  "/"+@italic-c
-     `italic-end`  ←  "/"+@(italic-c)
-     `non-italic`  ←  ( WS+ (!bold !italic-end fill)+
-                       / WS* bold
-                       / (!bold !italic-end fill)+ )+
+              italic  ←  italic-start italic-body italic-end
+      `italic-start`  ←  "/"+@italic-c !WS
+        `italic-end`  ←  "/"+@(italic-c)
+       `italic-body`  ←  ( WS+ (!non-italic !italic-end fill)+
+                          / WS* non-italic
+                          / (!non-italic !italic-end fill)+ )+
+       `non-italic`   ←  bold / strike / underline / literal
 
-           `fill`  ←  !WS 1
-             `WS`  ←  (" " / "\n")
-             raw   ←  (!bold !italic 1)+
+              strike  ←  strike-start strike-body strike-end
+      `strike-start`  ←  "~"+@strike-c !WS
+        `strike-end`  ←  "~"+@(strike-c)
+       `strike-body`  ←  ( WS+ (!non-strike !strike-end fill)+
+                                / WS* non-strike
+                                / (!non-strike !strike-end fill)+ )+
+        `non-strike`  ←  bold / italic / underline / literal
+
+           underline  ←  underline-start underline-body underline-end
+   `underline-start`  ←  "_"+@underline-c !WS
+     `underline-end`  ←  "_"+@(underline-c)
+    `underline-body`  ←  ( WS+ (!non-underline !underline-end fill)+
+                             / WS* non-underline
+                             / (!non-underline !underline-end fill)+ )+
+     `non-underline`  ←  bold / italic / strike /literal
+
+            literal  ←  literal-start literal-body literal-end
+    `literal-start`  ←  "="+@literal-c
+      `literal-end`  ←  "="+@(literal-c)
+     `literal-body`  ←  (!literal-end 1)+
+
+              `fill`  ←  !WS 1
+                `WS`  ←  (" " / "\n")
+                raw   ←  ( !bold
+                            !italic
+                            !strike
+                            !literal
+                            !underline
+                            !escape 1 )+
 ]]
 return Peg(prose_str)
