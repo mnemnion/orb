e3a8fbfeec0be635cc70c149ca356f37d873f55f
Sam Atman <atmanistan@gmail.com>
2020-01-26 01:15:29 -0500

    Add escaping for special characters

--- src/orb/prose.lua
+++ src/orb/prose.lua
@@ -7,24 +7,26 @@
 
 local Peg = require "espalier:peg"
 local prose_str = [[
-            prose ←  (italic / bold / raw)+
+            prose  ←  (escape / italic / bold / raw)+
 
-             bold ←   bold-start non-bold bold-end
-     `bold-start` ←  "*"+@bold-c !(" " / "\n")
-       `bold-end` ←  "*"+@(bold-c)
-       `non-bold` ←  ( WS+ (!italic !bold-end fill)+
-                      / WS* italic
-                      / (!italic !bold-end fill)+ )+
+           escape  ←  "\\" {*/~_=}
 
-           italic ←  italic-start non-italic italic-end
-   `italic-start` ←  "/"+@italic-c
-     `italic-end` ←  "/"+@(italic-c)
-     `non-italic` ←  ( WS+ (!bold !italic-end fill)+
-                      / WS* bold
-                      / (!bold !italic-end fill)+ )+
+             bold  ←   bold-start non-bold bold-end
+     `bold-start`  ←  "*"+@bold-c !(" " / "\n")
+       `bold-end`  ←  "*"+@(bold-c)
+       `non-bold`  ←  ( WS+ (!italic !bold-end fill)+
+                       / WS* italic
+                       / (!italic !bold-end fill)+ )+
 
-           `fill` ←  !WS 1
-             `WS` ←  (" " / "\n")
-             raw  ←  (!bold !italic 1)+
+           italic  ←  italic-start non-italic italic-end
+   `italic-start`  ←  "/"+@italic-c
+     `italic-end`  ←  "/"+@(italic-c)
+     `non-italic`  ←  ( WS+ (!bold !italic-end fill)+
+                       / WS* bold
+                       / (!bold !italic-end fill)+ )+
+
+           `fill`  ←  !WS 1
+             `WS`  ←  (" " / "\n")
+             raw   ←  (!bold !italic 1)+
 ]]
 return Peg(prose_str)
