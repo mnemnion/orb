3e8b126601a229f05eb198a10a1c2d194597f2e4
Sam Atman <atmanistan@gmail.com>
2020-02-03 09:16:01 -0500

    End every list-line with line-end
    Also replaced the 'val' rule with another instance of 'text',
    so we can apply prose parsing to text rules.

--- src/orb/list-line.lua
+++ src/orb/list-line.lua
@@ -13,20 +13,19 @@ local anterm = require "anterm:anterm"
 local listline_str = [[
      list-line  ←  depth number* sep WS
                    (cookie / radio)*
-                   (key colon val / text) (cookie list-end)*
+                   (key colon text / text) cookie* list-end
          depth  ←  " "*
         number  ←  [0-9]+
            sep  ←  "-" / "."
         cookie  ←  "[" (!"]" 1)+ "]"
          radio  ←  "(" 1 ")" ; this should be one utf-8 character
            key  ←  (!":" 1)+
-         colon  ←  ":"
-           val  ←  ws+ (!WS 1) 1+
-          text  ←  ((!cookie 1)* "[" (!"]" 1)+ "]" !list-end (!cookie 1)+)+
-                /  (!cookie 1)+
+         colon  ←  ":" &(ws (!ws 1))
+          text  ←  ((!cookie 1)* "[" (!"]" 1)+ "]" (!list-end !cookie 1)+)+
+                /  (!cookie !list-end 1)+
             WS  ←  ws
           `ws`  ←  { \n}+
-      list-end  ←  "\n"+ (-1)
+      list-end  ←  "\n"* (-1)
 ]]
 
 
@@ -51,7 +50,7 @@ local function listline_fn(t)
        if match.last == t. last then
          -- label the match according to the rule
          match.id = t.id or "list_line"
-         --match.indent = match:select"sep"().last - match.first + 2
+         match.indent = match:select"sep"().last - match.first + 2
          return setmetatable(match, Listline)
        else
          match.id = t.id .. "_INCOMPLETE"
