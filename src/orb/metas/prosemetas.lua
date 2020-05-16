66e71032c1343a1b71443ec3ee6a1fff6fa3662f
Sam Atman <atmanistan@gmail.com>
2020-05-15 19:48:57 -1000

    Fixup complex bookended markup nodes
    Left in the 'test' version of the grammar names, to have a commit
    with them in it.

--- src/orb/metas/prosemetas.lua
+++ src/orb/metas/prosemetas.lua
@@ -12,37 +12,6 @@ local Prose_M = {}
 
 
 
-local bounds = { bold      = "*",
-                 italic    = "/",
-                 literal   = "=",
-                 verbatim  = "`",
-                 underline = "_",
-                 strike    = "~" }
-
-
-
-local byte = assert(string.byte)
-
-local function _fillGen(bookend)
-   local bound = byte(bounds[bookend])
-   return function(bounded)
-      if #bounded == 0 then
-         local span = bounded:span()
-         local count = 1
-         while true do
-            if byte(count + 1) ~= bound then
-               break
-            end
-            count = count + 1
-         end
-         -- make bookend nodes
-      else
-
-      end
-   end
-end
-
-
 
 
 
