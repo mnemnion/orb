c5e31dab9cdfd47665b3968e4817c3ad3ec9e6c0
Sam Atman <atmanistan@gmail.com>
2021-05-12 09:54:03 -1000

    Add _Bridge.orb_home

--- src/orb.lua
+++ src/orb.lua
@@ -34,6 +34,24 @@ local Orb = {}
 
 
 
+
+
+assert(_Bridge.bridge_home, "Missing bridge home")
+_Bridge.orb_home = _Bridge.bridge_home .. "/orb"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 Orb.lume = require "orb:lume/lume"
 
 
